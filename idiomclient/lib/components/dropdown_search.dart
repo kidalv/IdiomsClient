9fe2e4c46c2e1c115950e132fad17676f0dcfd40
Vladislavs Kirvels <kisats.vlad@gmail.com>
2021-02-25 23:49:31 +0200

    changed flags, added settings functionality

--- idiomclient/lib/components/dropdown_search.dart
+++ idiomclient/lib/components/dropdown_search.dart
@@ -1,150 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:idiomclient/components/flag_row.dart';
+import 'package:idiomclient/protos/models.pb.dart';
 
 class DropdownSearch extends StatefulWidget {
-  final Map<String, String> list;
-  DropdownSearch({Key key, this.list}) : super(key: key);
+  final List<LanguageReply> list;
+  final Function(LanguageReply) onSelect;
+  const DropdownSearch({Key key, this.list, this.onSelect}) : super(key: key);
 
   @override
   _DropdownSearchState createState() => _DropdownSearchState(list);
 }
 
 class _DropdownSearchState extends State<DropdownSearch> {
-  Iterable<MapEntry<String, String>> tempList;
-  Map<String, String> allItems;
-  Map<String, String> selectedLanguages;
+  List<LanguageReply> tempList;
+  List<LanguageReply> allItems;
   bool collapsed = true;
   _DropdownSearchState(this.allItems) {
-    tempList = allItems.entries;
-    selectedLanguages = Map<String, String>();
+    tempList = allItems;
   }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey.withOpacity(0.1))),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                        child: Icon(Icons.search, color: Colors.grey[400]),
+    final width = MediaQuery.of(context).size.width;
+    final theme = Theme.of(context);
+    return Material(
+      borderRadius: BorderRadius.circular(10),
+      elevation: collapsed ? 0 : 6,
+      child: Container(
+        decoration: BoxDecoration(
+            color: theme.scaffoldBackgroundColor,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.grey.withOpacity(0.1))),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: Icon(Icons.search, color: Colors.grey[400]),
+                    ),
+                    SizedBox(
+                      width: width * 0.45,
+                      child: TextField(
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            hintText: "language"),
+                        onTap: () {
+                          setState(() {
+                            collapsed = false;
+                          });
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            tempList = allItems
+                                .where((x) => x.name.toLowerCase().contains(value.toLowerCase()))
+                                .toList();
+                          });
+                        },
+                        onSubmitted: (value) {},
                       ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.45,
-                        child: TextField(
-                          decoration: new InputDecoration(
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                              hintText: "language"),
-                          onTap: () {
-                            setState(() {
-                              collapsed = false;
-                            });
-                          },
-                          onChanged: (value) {
-                            setState(() {
-                              tempList = allItems.entries.where((x) => x.key.contains(value));
-                            });
-                          },
-                          onSubmitted: (value) {},
-                        ),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                      padding: EdgeInsets.zero,
-                      splashRadius: 25,
-                      icon: Icon(
-                        collapsed ? Icons.arrow_drop_down : Icons.arrow_drop_up,
-                        color: Colors.grey[400],
-                        size: 30,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          collapsed = !collapsed;
-                        });
-                      })
-                ],
-              ),
-              !collapsed
-                  ? Container(
-                      padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                      height: MediaQuery.of(context).size.height * 0.2,
-                      child: Scrollbar(
-                        thickness: 5,
-                        radius: Radius.circular(10),
-                        controller: ScrollController(),
-                        child: ListView(
-                          padding: EdgeInsets.only(left: 32),
-                          physics: BouncingScrollPhysics(),
-                          children: tempList
-                              .map((e) => LanguageRow(
-                                    language: e.key,
-                                    code: e.value,
-                                    onPress: () {
-                                      setState(() {
-                                        selectedLanguages[e.key] = e.value;
-                                        allItems.removeWhere((key, value) => value == e.value);
-                                        tempList = tempList.where((x) => x.value != e.value);
-                                      });
-                                    },
-                                  ))
-                              .toList(),
-                        ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                    padding: EdgeInsets.zero,
+                    splashRadius: 25,
+                    icon: Icon(
+                      collapsed ? Icons.arrow_drop_down : Icons.arrow_drop_up,
+                      color: Colors.grey[400],
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        collapsed = !collapsed;
+                      });
+                    })
+              ],
+            ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+              width: width * 0.68,
+              height: collapsed ? 0 : MediaQuery.of(context).size.height * 0.3,
+              child: !collapsed
+                  ? Scrollbar(
+                      thickness: 5,
+                      radius: const Radius.circular(10),
+                      controller: ScrollController(),
+                      child: ListView(
+                        padding: const EdgeInsets.only(left: 32),
+                        physics: const BouncingScrollPhysics(),
+                        children: tempList
+                            .map((x) => LanguageRow(
+                                  language: x.name,
+                                  code: x.region,
+                                  onPress: () {
+                                    widget.onSelect(x);
+                                    setState(() {
+                                      allItems.remove(x);
+                                      tempList = tempList.where((y) => y.name != x.name).toList();
+                                    });
+                                  },
+                                ))
+                            .toList(),
                       ),
                     )
                   : Container(),
-            ],
-          ),
-        ),
-        if (selectedLanguages.isNotEmpty)
-          Container(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 10),
-              child: const Text("Selected languages: ")),
-        if (selectedLanguages.isNotEmpty)
-          Container(
-            padding: const EdgeInsets.all(5.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.grey.withOpacity(0.1))),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Wrap(
-                children: selectedLanguages.entries
-                    .map((e) => InkWell(
-                          onTap: () {
-                            setState(() {
-                              selectedLanguages.removeWhere((x, y) => y == e.value);
-                              allItems[e.key] = e.value;
-                              tempList = allItems.entries;
-                            });
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(5.0),
-                            width: 44,
-                            child: FlagRow(
-                              code: e.value,
-                              length: 30,
-                              flagOnly: true,
-                            ),
-                          ),
-                        ))
-                    .toList(),
-              ),
             ),
-          )
-      ],
+          ],
+        ),
+      ),
     );
   }
 }
